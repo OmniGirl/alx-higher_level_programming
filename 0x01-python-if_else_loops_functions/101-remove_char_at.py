2478da5f42606e6f3fdcce53a09ea4b1861c48a9
OmniGirl <okang.etta@gmail.com>
2023-05-09 18:59:16 -0700

    Python If-Else-Loops-Functions

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    # Get the length of the string
+    length = len(str)
+    # If n is out of range, return the original string
+    if n < 0 or n >= length:
+        return str
+    # Create a new string by concatenating the parts before and after the nth character
+    return str[:n] + str[n+1:]
