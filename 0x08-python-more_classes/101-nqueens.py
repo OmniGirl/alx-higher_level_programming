1180e42c738900ad9d4d0302d170fa8915352aac
OmniGirl <okang.etta@gmail.com>
2023-06-05 19:46:40 -0700

    update

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,58 +1,57 @@
 #!/usr/bin/python3
 """
-Solution to the N-queens puzzle.
-Determines all possible solutions to placing N
-N non-attacking queens on an NxN chessboard
+nqueens backtracking program to print the coordinates of n queens
+on an nxn grid such that they are all in non-attacking positions
 """
 from sys import argv
 if __name__ == "__main__":
-    ab = []
+    a = []
     if len(argv) != 2:
         print("Usage: nqueens N")
         exit(1)
     if argv[1].isdigit() is False:
         print("N must be a number")
         exit(1)
-    h = int(argv[1])
-    if h < 4:
+    n = int(argv[1])
+    if n < 4:
         print("N must be at least 4")
         exit(1)
     # initialize the answer list
-    for z in range(h):
-        ab.append([z, None])
+    for i in range(n):
+        a.append([i, None])
 
-    def already_exists(q):
-        """check that a queen does not already exist in that f value"""
-        for f in range(h):
-            if q == ab[f][1]:
+    def already_exists(y):
+        """check that a queen does not already exist in that y value"""
+        for x in range(n):
+            if y == a[x][1]:
                 return True
         return False
 
-    def reject(f, q):
+    def reject(x, y):
         """determines whether or not to reject the solution"""
-        if (already_exists(q)):
+        if (already_exists(y)):
             return False
-        z = 0
-        while(z < f):
-            if abs(ab[z][1] - q) == abs(z - f):
+        i = 0
+        while(i < x):
+            if abs(a[i][1] - y) == abs(i - x):
                 return False
-            z += 1
+            i += 1
         return True
 
-    def clear_a(f):
+    def clear_a(x):
         """clears the answers from the point of failure on"""
-        for z in range(f, h):
-            ab[z][1] = None
+        for i in range(x, n):
+            a[i][1] = None
 
-    def nqueens(f):
+    def nqueens(x):
         """recursive backtracking function to find the solution"""
-        for q in range(h):
-            clear_a(f)
-            if reject(f, q):
-                ab[f][1] = q
-                if (f == h - 1):  # accepts the solution
-                    print(ab)
+        for y in range(n):
+            clear_a(x)
+            if reject(x, y):
+                a[x][1] = y
+                if (x == n - 1):  # accepts the solution
+                    print(a)
                 else:
-                    nqueens(f + 1)  # moves on to next f value to continue
-    # start the recursive process at f = 0
+                    nqueens(x + 1)  # moves on to next x value to continue
+    # start the recursive process at x = 0
     nqueens(0)
