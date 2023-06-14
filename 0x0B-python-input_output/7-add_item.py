ca368c7c48ae9dd898f3c6a58b5bce36f89d1f31
OmniGirl <okang.etta@gmail.com>
2023-06-14 09:03:53 -0700

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-"""Handles file input and saves to file"""
-from sys import argv
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+"""
+    This is a python script that adds all arguments to a Python List.
+    List is then saved to a file.
+"""
+
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-argv.pop(0)
-try:
-    deserialized = load_from_json_file("add_item.json")
-    if deserialized is None:
-        save_to_json_file(argv, "add_item.json")
-    else:
-        deserialized.extend(argv)
-        save_to_json_file(deserialized, "add_item.json")
-except FileNotFoundError:
-    save_to_json_file(argv, "add_item.json")
+if __name__ == "__main__":
+
+    filename = "add_item.json"
+
+    try:
+        arg_list = load_from_json_file(filename)
+    except Exception:
+        arg_list = []
+
+    for arg in sys.argv[1:]:
+        arg_list.append(arg)
+    save_to_json_file(arg_list, filename)
